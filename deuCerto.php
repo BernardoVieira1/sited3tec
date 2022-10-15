677153ca8e973f3095fc40f06866dfbe0d07fc96
LucasMathewws <lucasmathews577@gmail.com>
2022-10-14 22:20:41 -0300

    salve

--- deuCerto.php
+++ deuCerto.php
@@ -16,5 +16,22 @@
   }
 
 ?>
-<br>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Iron Maiden World Tour 2023</title>
+  <meta name = "viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+</head>
+<body>
+  <header>
+  <div class="logo"><ing src="https://i0.wp.com/www.xsnoize.com/wp-content/uploads/2022/10/5f4716cf-e259-08cb-5c1b-467b25aaa2b5_edited.jpg" /></div> 
+
+  </header>
+
+
 <a href="sair.php"><button>Sair</button></a>
+</body>
+</html>
